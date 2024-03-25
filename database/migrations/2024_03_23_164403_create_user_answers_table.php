e223c683befd9979e9ed40dfb3f854c8413aa52a
roman <romanrus98@mail.ru>
2024-03-25 20:48:46 +0500

    feat: table answers

---
+++ database/migrations/2024_03_23_164403_create_user_answers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('test_user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('question_id')->constrained()->cascadeOnDelete();
+            $table->json('answers');
+            $table->boolean('correct')->nullable();
+            $table->unique(['test_user_id', 'question_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_answers');
+    }
+};
