6166df53d0fb22719a8a735612efcbea7b617f8a
roman <romanrus98@mail.ru>
2024-03-25 20:49:02 +0500

    feat: add asnwers

--- app/Models/TestUser.php
+++ app/Models/TestUser.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TestUser extends Model
 {
@@ -22,5 +23,10 @@ class TestUser extends Model
     public function questions(): BelongsToMany
     {
         return $this->belongsToMany(Question::class, 'test_user_questions', 'test_user_id', 'question_id')->withTimestamps();
+    }
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(UserAnswer::class);
     }
 }
